5d26b3581f8482ab2ee52b49980adf35c91a8c6b
KaviAiArts <rajvishwakarma2242@gmail.com>
2025-12-21 21:44:08 +0530

    New Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { HelmetProvider } from "react-helmet-async";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <App />
+    <SpeedInsights />
   </HelmetProvider>
 );
