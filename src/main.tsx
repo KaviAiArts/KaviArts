6a98c98dc225e69b0848d5ab7faf56d8b8a41d59
KaviAiArts <rajvishwakarma2242@gmail.com>
2026-02-04 02:50:49 +0530

    main.tsx changed for vercel files deletes

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { HelmetProvider } from "react-helmet-async";
-import { SpeedInsights } from "@vercel/speed-insights/react"; // ⚡ Performance
-import { Analytics } from "@vercel/analytics/react";       // 📊 Visitor Views
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <App />
-    <SpeedInsights />
-    <Analytics />
   </HelmetProvider>
 );
