d64d75d3ebeae56a8265b1dcb3cf1514d840461c
KaviAiArts <rajvishwakarma2242@gmail.com>
2026-02-09 23:19:16 +0530

    fix SSR build by removing react-helmet-async (vite-ssg safe)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ViteSSG } from "vite-plugin-ssg";
+import App from "./App";
 import "./index.css";
 
-import { HelmetProvider } from "react-helmet-async";
-
-createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+export const createApp = ViteSSG(
+  App,
+  { routes: [] },
+  ({ app }) => {
+    // no helmet provider – SSR safe
+  }
 );
+
+if (typeof window !== "undefined") {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
